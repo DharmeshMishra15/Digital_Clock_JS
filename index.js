56a9287b66d58356d95fe4f81d83ba97760607fd
DharmeshMishra15 <96678907+DharmeshMishra15@users.noreply.github.com>
2023-02-08 22:00:03 +0530

    Add files via upload
    commit changes

---
+++ index.js
@@ -0,0 +1,38 @@
+function display()
+{
+    var dateTime=new Date();
+    var hrs= dateTime.getHours();
+    var min= dateTime.getMinutes();
+    var sec= dateTime.getSeconds();
+    var session=document.getElementById("session");
+    
+    if(hrs=>12)
+    {
+        session="PM";
+        hrs-=12;
+    }
+    else
+    {
+        session="AM";
+    }
+    document.getElementById('hours').innerHTML=hrs;
+    document.getElementById('minutes').innerHTML=min;
+    document.getElementById('seconds').innerHTML=sec; 
+    document.getElementById('session').innerHTML=session;
+
+
+
+}
+
+setInterval(display,10);
+
+
+function over()
+{
+    document.getElementsByTagName("body")[0].style.backgroundColor="#698269";
+}
+
+function out()
+{
+    document.getElementsByTagName("body")[0].style.backgroundColor="#A7727D";
+}
